0ab767d0d7291af1d95a15b10cb52f4aa8ea4043
Eric Laursen <ericjlaursen@gmail.com>
2018-03-30 22:28:08 -0400

    removes dev lines.

--- server.js
+++ server.js
@@ -13,10 +13,4 @@ app.listen(PORT, () => {
   console.log(`Your app is listening on port ${PORT}`);
 });
 
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
-if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
-
 module.exports = {app};
